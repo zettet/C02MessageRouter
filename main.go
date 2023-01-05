028578676d7bd6e9aaec3facb4ad4cae9f46e36c
zettet <tzettersten@gmail.com>
2023-01-05 17:40:05 -0600

    minor fixes for building application and some debug output

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package EmissionsMessageRouter
+package main
 
 import (
 	"EmissionsMessageRouter/clients"
@@ -9,32 +9,39 @@ import (
 )
 
 func main() {
-
+	fmt.Println("starting up processor")
 	for true {
+		fmt.Println("creating connection")
 		connection, err := clients.GetTCPConnection()
 		if err != nil {
-			fmt.Errorf("Error obtaining tcp connection: %v", err)
+			fmt.Println(fmt.Sprintf("Error obtaining tcp connection: %v", err))
 		} else {
-			handleMessage(connection)
+			fmt.Println("received connection")
+			err = handleMessage(connection)
+			if err != nil {
+				fmt.Println(fmt.Sprintf("Error handling message: %v", err))
+			}
 		}
 
 		// sleeping here so that there is some time in between requests, mainly so that terminal output can be easily read
+		fmt.Println("sleeping for 10 seconds...")
 		time.Sleep(10 * time.Second)
 	}
-
 }
 
-func handleMessage(connection *net.TCPConn) {
+func handleMessage(connection *net.TCPConn) error {
 	messageBytes, err := clients.FetchTCPData(connection)
+	fmt.Println("Received message bytes")
 	if err != nil {
-		fmt.Errorf("Error fetching data from tcp connection: %v", err)
+		return fmt.Errorf("Error fetching data from tcp connection: %v", err)
 	}
 
 	emissionsMessage, err := bytes.ParseMessage(messageBytes)
 	if err != nil {
-		fmt.Errorf("Error parsing message bytes: %v", err)
+		return fmt.Errorf("Error parsing message bytes: %v", err)
 	}
 
 	// could have a prettier output here...
 	fmt.Println(emissionsMessage)
+	return nil
 }
